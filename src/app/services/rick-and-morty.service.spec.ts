e6ce408a92c30d929cde0fb1c01cc0a8e7bf5b00
ctomrp <cristian.rico.palma@gmail.com>
2023-10-23 22:45:15 -0300

    pruebas unitarias

--- src/app/services/rick-and-morty.service.spec.ts
+++ src/app/services/rick-and-morty.service.spec.ts
@@ -1,16 +1,45 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RickAndMortyService } from './rick-and-morty.service';
 
 describe('RickAndMortyService', () => {
   let service: RickAndMortyService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [RickAndMortyService],
+    });
+
     service = TestBed.inject(RickAndMortyService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve characters from the API via GET', () => {
+    const mockResponse = {
+      results: [
+        {
+          id: 1,
+          name: 'Rick Sanchez'
+        }
+      ]
+    };
+
+    service.getCharacters().subscribe((data) => {
+      expect(data).toEqual(mockResponse);
+    });
+
+    const req = httpTestingController.expectOne(service['apiUrl']);
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockResponse);
   });
 });
